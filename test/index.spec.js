d80c4b91c10152a3287a2863b0f04d42c2fece02
Tomas Miliauskas <tomas@wix.com>
2018-03-27 09:44:34 +0300

    Rename in tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,45 +2,45 @@ const {accessSync, constants, readFileSync, writeFileSync} = require('fs');
 const path = require('path');
 const rimraf = require('rimraf');
 const glob = require('glob');
-const tempura = require('../src');
+const cista = require('../src');
 
-describe('tempura', () => {
+describe('cista', () => {
   it('should create empty dir in tmp', () => {
-    const {dir, cleanup} = tempura();
+    const {dir, cleanup} = cista();
     expect(exists(dir)).toBe(true);
     cleanup();
   });
 
   it('should generate project with few files', () => {
     const tree = {'src/file.js': ';', 'test/test.js': ';', 'package.json': '{}'};
-    const {dir, cleanup} = tempura(tree);
+    const {dir, cleanup} = cista(tree);
     expect(listFiles(dir)).toEqual(expect.arrayContaining(Object.keys(tree)));
     cleanup();
   });
 
   it('should generate project when passing tree', () => {
     const tree = {src: {'file.js': ';'}};
-    const {dir, cleanup} = tempura(tree);
+    const {dir, cleanup} = cista(tree);
     expect(listFiles(dir)).toEqual(['src/file.js']);
     cleanup();
   });
 
   describe('.files', () => {
     it('should return the content of a file', () => {
-      const {dir, files, cleanup} = tempura({'file.js': ';'});
+      const {dir, files, cleanup} = cista({'file.js': ';'});
       const fileContent = readFile(path.join(dir, 'file.js'));
       expect(files['file.js']).toBe(fileContent);
       cleanup();
     });
 
     it('should return undefined for file which doesn\'t exist', () => {
-      const {files, cleanup} = tempura();
+      const {files, cleanup} = cista();
       expect(files['file.js']).toBeUndefined();
       cleanup();
     });
 
     it('should change the content of a file', () => {
-      const {dir, files, cleanup} = tempura({'file.js': ';'});
+      const {dir, files, cleanup} = cista({'file.js': ';'});
       files['file.js'] = 'break;';
       const fileContent = readFile(path.join(dir, 'file.js'));
       expect(files['file.js']).toBe(fileContent);
@@ -48,7 +48,7 @@ describe('tempura', () => {
     });
 
     it('should return the whole list of files', () => {
-      const {dir, files, cleanup} = tempura();
+      const {dir, files, cleanup} = cista();
       const file = writeFileSync(path.join(dir, 'file.js'), ';');
       expect(files.list).toEqual(['file.js']);
       cleanup();
@@ -57,7 +57,7 @@ describe('tempura', () => {
 
   describe('.cleanup()', () => {
     it('should remove directory from tmp', () => {
-      const {dir, cleanup} = tempura();
+      const {dir, cleanup} = cista();
       cleanup();
       expect(exists(dir)).toBe(false);
     });
